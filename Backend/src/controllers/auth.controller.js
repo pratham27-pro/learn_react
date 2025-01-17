d22141d74e57329ad3fa235ec14824ccdeefde57
Pratham Jain <2muchappy420@gmail.com>
2025-01-17 23:52:39 +0530

    Login functionailty

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import AuthUser from "../models/auth-user.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -13,5 +14,28 @@ export const signup = async (req, res, next) => {
         .json({ message: "User signup successfull!!"});
     } catch (error) {
         next(errorHandler(300, "Error while doing user signup!"));
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await AuthUser.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(401, "Invalid email/credentials"));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);  
+        if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest} = validUser._doc();
+
+        const expiry = new Date(Date.now() + 3600000); // 1hr
+        res.cookie("access-token", token, {httpOnly: true, expires: expiry}).status(200).json(validUser);
+
+    } catch (error) {
+        next(error);
+        console.log("Error while login of the user");
     }
 }
