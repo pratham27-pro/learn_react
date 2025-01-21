f3a8fc808384ceebf7282e4d4f0068d909f55cde
Pratham Jain <2muchappy420@gmail.com>
2025-01-21 23:35:04 +0530

    try11

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/', // Adjust this if your app is hosted in a subdirectory
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
     sourcemap: true,
   },
   plugins: [react()],
   server: {
-    sourcemap: true, // Ensure source maps are enabled for the deployment
-    target: process.env.NODE_ENV === 'production' 
-          ? 'https://eazypg-backend.onrender.com'  // Deployed backend URL on Render
-          : 'http://localhost:8000',  
-  }
-})
+    proxy: {
+      // Proxy API calls during development
+      '/api': {
+        target: process.env.NODE_ENV === 'production'
+          ? 'https://eazypg-backend.onrender.com' // Deployed backend URL
+          : 'http://localhost:8000', // Local backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
